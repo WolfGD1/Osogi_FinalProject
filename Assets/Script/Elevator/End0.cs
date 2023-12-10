1a66f0a9d744878a7592705d2fdbb269a0810d21
KwakCM0608 <byj010608@naver.com>
2023-12-11 03:28:51 +0900

    third step

---
+++ Assets/Script/Elevator/End0.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End0 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    public void startClick(){
+        SceneManager.LoadScene("Game0");
+    }
+    public void helpClick(){
+        SceneManager.LoadScene("Start1");
+    }
+}
